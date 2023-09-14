e01ba5c950a3c7c9faf7c04ab9293c029a470d6f
fe1w0 <xzasliuxinrong@gmail.com>
2023-09-14 22:29:35 +0800

    2023/09/14, Fuzzing example.jar.

--- src/test/java/xyz/xzaslxr/driver/FuzzChainsDriver.java
+++ src/test/java/xyz/xzaslxr/driver/FuzzChainsDriver.java
@@ -58,7 +58,7 @@ public class FuzzChainsDriver {
     public static void main(String[] args) throws IOException {
         // 设置启动参数
 
-        String fuzzTime = "3000s";
+        String fuzzTime = "30s";
         String isSkipException = "false";
         String fuzzGuidance = "ZEST";
         // String fuzzMode = "chains";
@@ -70,7 +70,7 @@ public class FuzzChainsDriver {
 
         String inputFilePath = null;
 
-        Long trials = 10000L;
+        Long trials = 100L;
 
         String fuzzTargetDirectory = "/Users/fe1w0/Project/SoftWareAnalysis/Dynamic/FuzzChains/DataSet/targets/xyz-xzaslxr-1.0.jar";
 
